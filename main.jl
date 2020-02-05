66fab7f8bb13ac93fa94499de91bc4ce57d62395
Guru Vamsi Policharla <guruvamsi.policharla@gmail.com>
2020-02-05 18:34:57 +0530

    Build replicates code sent from TIFR

--- main.jl
+++ main.jl
@@ -1,51 +1,29 @@
 using QuantumOptics
-
-#=
-    Input:
-        fq : frequency of Qubit
-        fc : frequency of Cavity
-        fd:  cavity drive frequency
-        ah1: Anharmonicity of Qubit
-        g_qc : Qubit-Cavity coupling
-        kappa_c: line width of the cavity
-
-        Cavity drive of type  A1*cos(wt+phi)
-        A1: Amplitude of drive
-        phi: phase of drive
-        t:time length of drive
-        ts:number of point per time unit.
-	    t_cutoff:time after which drive stops
-
-    Output:
-        defines following global variables like,
-        aq,Npq,ac,Npc : annihilation and number operator of Q1
-        H0     : wq*Nq + (ah/2)*Nq*(1-Nq) + w1*Nq1
-        H1     : (a + a.dag())
-        c_op_list: list containing collapse operators iff Tq is non-zero
-                   Only 1-->0 decays is considered. (Sinlge photon decay)
-
-        returns density matrix for given tlist
-=#
+using DifferentialEquations
+using JLD2
+using PyPlot
 
 function instate(Nq,q_state_amp,Nc)
 #Nq = levels in the qubit
 #state_amp = amplitudes of the state
 #Nc = levels in the cavity
+    #=
     if mod(Nq,2) == 0
         q_basis = SpinBasis((Nq-1) // 2)
     else
         q_basis = SpinBasis((Nq-1)/2 // 1)
     end
-
+    =#
+    q_basis = FockBasis(Nq)
     c_basis = FockBasis(Nc)
     ψ0 = Ket(q_basis, q_state_amp) ⊗ fockstate(c_basis,0)
 
     return q_basis, c_basis, ψ0
 end
 
-function H(t, ρ) # time-dependent Hamiltonian
+function H(t, ρ=0) # time-dependent Hamiltonian
     if t<t_cutoff
-        H = H0 + A*cos(ωd*t + ϕ)*Iqc
+        H = H0 + -1*A*cos(ωd*t + ϕ)*Xc
     else
         H = H0
     end
@@ -53,7 +31,9 @@ function H(t, ρ) # time-dependent Hamiltonian
     return H, J, Jdagger
 end
 
-q_basis, c_basis, ψ0 = instate(6,[0,1,0,0,0,0],20)
+nq = 5
+nc = 9
+q_basis, c_basis, ψ0 = instate(nq,[0,1,0,0,0,0],nc)
 
 fq = 5.4
 fc = 6.78
@@ -68,26 +48,30 @@ kappa_q = 0.02
 ah = ah1*2.0*pi      # qubit anharmonicity
 g  = g_qc*2.0*pi     # qubit cavity coupling const
 A  = 2.0*pi*0.1      # drive amplitude
-ωd = 2.0*pi*6.78     # drive frequency
+ωd = 2.0*pi*fd     # drive frequency
 ϕ = 0                # phase of drive
 t_cutoff = 60       # drive cutoff time
 
-aq = sigmam(q_basis) ⊗ identityoperator(c_basis)    # qubit annihilation op
+aq = destroy(q_basis) ⊗ identityoperator(c_basis)    # qubit annihilation op
 ac = identityoperator(q_basis) ⊗ destroy(c_basis)    # cavity annihilation op
 
-Iqc = identityoperator(q_basis) ⊗ identityoperator(c_basis) # Identity operator
+Xq = (aq + dagger(aq)); Npq = dagger(aq)*aq;
+Xc = (ac + dagger(ac)); Npc = dagger(ac)*ac;
 
-Xq = (aq + dagger(aq)); Yq = -1*im*(aq - dagger(aq)); Zq = (Iqc - 2*dagger(aq)*aq); Npq = dagger(aq)*aq;
-Xc = (ac + dagger(ac)); Yc = -1*im*(ac - dagger(ac)); Zc = (Iqc - 2*dagger(ac)*ac); Npc = dagger(ac)*ac;
-
-Hq = ωq*Npq + (ah/2)*Npq*(Iqc-Npq)    # qubit Duffing oscillator model
+Hq = ωq*Npq + (ah/2)*(Npq-Npq*Npq)    # qubit Duffing oscillator model
 Hc = ωc*Npc    # cavity - harmonic oscillator
 Hqc = g*(Xq)*(Xc)    # qubit cavity interaction
 Hd = Xc    # cavity drive
 H0 = Hq + Hc + Hqc    #time independent part of Hamiltonian
 
-J = [sqrt(kappa_c)*ac,sqrt(kappa_q)*aq]
+J = [sqrt(kappa_c)*ac, sqrt(kappa_q)*aq]
 Jdagger = dagger.(J)
 #R = [kappa_c,kappa_q]
-T = 1:0.025:100
-@time tout, ρ = timeevolution.master_dynamic(T, ψ0, H)
+T = 0:0.01:100
+
+println(Threads.nthreads())
+@time tout, ρ = timeevolution.master_dynamic(T, ψ0, H;alg=OrdinaryDiffEq.Vern7())
+@save "data "*string(nq)*","*string(nc)*","*string(maximum(T))*".jld2" tout ρ T
+pygui(true)
+plot(tout, (expect(Npc, ρ)))
+grid("on")
